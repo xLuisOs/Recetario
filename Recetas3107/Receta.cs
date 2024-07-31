5c8cfccaa337cabec05e0ea40f9bd4c2069eeac1
Luis Granados <luisoswaldogra@gmail.com>
2024-07-31 09:20:02 -0600

    Add project files.

---
+++ Recetas3107/Receta.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recetas3107
+{
+    internal class Receta
+    {
+        
+
+        public string Nombre { get; set; }
+        public string Ingredientes { get; set; }
+        public string Procedimiento { get; set; }
+        public Receta(string nombre, string ingredientes, string procedimiento)
+        {
+            Nombre = nombre;
+            Ingredientes = ingredientes;
+            Procedimiento = procedimiento;
+        }
+    }
+}
